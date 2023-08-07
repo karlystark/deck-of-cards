c672e29d0867f52e38ffc8056eb562d59f6feb73
Karly Stark <karlystark@gmail.com>
2023-08-07 16:39:02 -0700

    completed part one

--- numberFacts.js
+++ numberFacts.js
@@ -3,7 +3,8 @@
 const BASE_URL = "http://numbersapi.com"
 
 /**
- *
+ * Makes call for single fact from NumbersAPI and appends fact to page
+ * Accepts: one number
  */
 async function getFavNumberFact(num){
 
@@ -13,12 +14,15 @@ async function getFavNumberFact(num){
     headers: {"content-type": "application/json"}
   })
 
-  $(".single-fact").text(resp.data);
+  const fact = resp.data;
+
+  $(".single-fact").append(`<p>${fact}</p>`);
 }
 
 
 /**
- *
+ * Makes call for batch of facts from NumbersAPI and appends facts to page
+ * Accepts: min, max numbers
  */
 async function getBatchFacts(min, max){
 
@@ -28,12 +32,64 @@ async function getBatchFacts(min, max){
     headers: {"content-type": "application/json"}
   })
 
-  for (let key in resp.data){
-    $(".multiple-facts").append(`<p>${resp.data[key]}</p>`)
+  const factsBatch = resp.data;
+
+  for (let key in factsBatch){
+    $(".multiple-facts").append(`<p>${factsBatch[key]}</p>`)
   }
 }
 
+/**
+ * Makes four calls to NumbersAPI and appends facts to page
+ * Accepts: one number
+ */
+async function getFourFacts(num){
 
+  const f1 = axios({
+    method: "GET",
+    url: `${BASE_URL}/${num}`,
+    headers: {"content-type": "application/json"}
+  })
+
+  const f2 = axios({
+    method: "GET",
+    url: `${BASE_URL}/${num}`,
+    headers: {"content-type": "application/json"}
+  })
+
+  const f3 = axios({
+    method: "GET",
+    url: `${BASE_URL}/${num}`,
+    headers: {"content-type": "application/json"}
+  })
+
+  const f4 = axios({
+    method: "GET",
+    url: `${BASE_URL}/${num}`,
+    headers: {"content-type": "application/json"}
+  })
+
+  const resolvedPromises = await Promise.allSettled([f1, f2, f3, f4]);
+  //Promise.all => this will give all or none promises depending on them all succeeding
+
+  for (let result of resolvedPromises){
+       $(".four-facts").append(`<p>${result.value.data}</p>`)
+      console.log(result);
+    }
+}
+
+/**
+ * Calls API functions and displays results on page.
+ */
+async function displayFacts(){
+
+await getFavNumberFact(11);
+await getBatchFacts(5,8);
+await getFourFacts(7);
+
+}
+
+displayFacts();
 
 
 
