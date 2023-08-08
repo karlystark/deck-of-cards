c4376331bde88709d9eaac3e531322435f64c8b8
Karly Stark <karlystark@gmail.com>
2023-08-07 19:36:47 -0700

    deck draw complete

--- deckOfCards.js
+++ deckOfCards.js
@@ -2,13 +2,10 @@
 
 const BASE_URL = "https://deckofcardsapi.com/api/deck"
 let deckID;
-// class Deck {
-//   constructor(){
-//     this.deckID = this.
-//   }
 
-// }s
-
+/*
+** Fetch shuffled deck from API and set deckID
+*/
 async function fetchAndSetDeck() {
 
   const resp = await axios({
@@ -20,45 +17,82 @@ async function fetchAndSetDeck() {
 
 }
 
+/*
+** Call API to pull a single card, append card to canvas and apply transforms
+** to each card. If deck empty, display refresh message.
+*/
 async function pullSingleCard() {
-  //await fetchAndSetDeck();
 
   const resp = await axios({
     method: "GET",
     url: `${BASE_URL}/${deckID}/draw/?count=1`
   });
 
-  const cardValue = resp.data.cards[0].value;
-
-  //console.log(cardValue);
+  const cardImg = resp.data.cards[0].image;
   const cardSuit = resp.data.cards[0].suit;
-  // console.log(cardSuit);
+  const cardValue = resp.data.cards[0].value;
+  const cardCode = resp.data.cards[0].code;
 
-   console.log(`${cardValue} of ${cardSuit}`);
+  const randRotation = Math.floor(Math.random() * 100) -50;
+  const randTranslate = Math.floor(Math.random() * 50) - 25;
+
+  $(".card-canvas").append(
+    `<img src="${cardImg}" alt="${cardValue} of ${cardSuit}" class="card card-${cardCode}">`
+  );
+
+  $(`.card-${cardCode}`).css(
+    "transform",
+    `rotate(${randRotation}deg)
+    translateY(${randTranslate}px)
+    translateX(${randTranslate}px)`
+  );
+
+  if (resp.data.remaining === 0) {
+     $(".draw-card-btn").text("REFRESH TO SHUFFLE DECK");
+  }
+
 }
 
-async function pullTwoCards(){
-
-  //await fetchAndSetDeck();
-
-  const card1 =  axios({
-    method: "GET",
-    url: `${BASE_URL}/${deckID}/draw/?count=1`
-  });
-
-  const card2 = axios({
-    method: "GET",
-    url: `${BASE_URL}/${deckID}/draw/?count=1`
-  });
-
-  const resolvedPromises = await Promise.allSettled([card1, card2]);
-  console.log(resolvedPromises);
-
-   for(let card of resolvedPromises){
-     console.log(
-      `${card.value.data.cards[0].value} of ${card.value.data.cards[0].suit}`
-    );
-   }
+/*
+** Await fetchAndSetDeck at start.
+*/
+async function start(){
+  await fetchAndSetDeck();
 }
+
+
+$(".draw-card-btn").on("click", pullSingleCard);
+
+start();
+
+
+
+/* code from previous exercises:
+
+// async function pullTwoCards(){
+
+//   //await fetchAndSetDeck();
+
+//   const card1 =  axios({
+//     method: "GET",
+//     url: `${BASE_URL}/${deckID}/draw/?count=1`
+//   });
+
+//   const card2 = axios({
+//     method: "GET",
+//     url: `${BASE_URL}/${deckID}/draw/?count=1`
+//   });
+
+//   const resolvedPromises = await Promise.allSettled([card1, card2]);
+//   console.log(resolvedPromises);
+
+//    for(let card of resolvedPromises){
+//      console.log(
+//       `${card.value.data.cards[0].value} of ${card.value.data.cards[0].suit}`
+//     );
+//    }
+// }
+
+*/
 
 
