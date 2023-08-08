5754ab34c99121bbb2a29f4bcb1183ffa34e60cb
Karly Stark <karlystark@gmail.com>
2023-08-07 17:58:53 -0700

    WIP part 2, completed numbers 1 and 2

--- deckOfCards.js
+++ deckOfCards.js
@@ -1,52 +1,64 @@
 "use strict";
 
 const BASE_URL = "https://deckofcardsapi.com/api/deck"
-const CURR_DECK = getDeck()
+let deckID;
+// class Deck {
+//   constructor(){
+//     this.deckID = this.
+//   }
 
+// }s
 
+async function fetchAndSetDeck() {
 
-class Deck {
-  constructor(){
-    this.deckID = this.getDeck()
-  }
+  const resp = await axios({
+    method: "GET",
+    url: `${BASE_URL}/new/shuffle/?deck_count=1`
+  });
 
-  static async getDeck() {
-
-    const resp = await axios({
-      method: "GET",
-      url: `${BASE_URL}/new/shuffle/?deck_count=1`
-    });
-
-    const deckID = resp.data.deck_id;
-
-    return deckID;
-  }
+  deckID = resp.data.deck_id;
 
 }
 
-const deck = new Deck()
+async function pullSingleCard() {
+  //await fetchAndSetDeck();
+
+  const resp = await axios({
+    method: "GET",
+    url: `${BASE_URL}/${deckID}/draw/?count=1`
+  });
+
+  const cardValue = resp.data.cards[0].value;
+
+  //console.log(cardValue);
+  const cardSuit = resp.data.cards[0].suit;
+  // console.log(cardSuit);
+
+   console.log(`${cardValue} of ${cardSuit}`);
+}
+
+async function pullTwoCards(){
+
+  //await fetchAndSetDeck();
+
+  const card1 =  axios({
+    method: "GET",
+    url: `${BASE_URL}/${deckID}/draw/?count=1`
+  });
+
+  const card2 = axios({
+    method: "GET",
+    url: `${BASE_URL}/${deckID}/draw/?count=1`
+  });
+
+  const resolvedPromises = await Promise.allSettled([card1, card2]);
+  console.log(resolvedPromises);
+
+   for(let card of resolvedPromises){
+     console.log(
+      `${card.value.data.cards[0].value} of ${card.value.data.cards[0].suit}`
+    );
+   }
+}
 
 
-// async function getDeck() {
-
-//   const resp = await axios({
-//     method: "GET",
-//     url: `${BASE_URL}/new/shuffle/?deck_count=1`
-//   });
-
-//   const deckID = resp.data.deck_id;
-
-//   return deckID;
-// }
-
-
-// async function drawCard() {
-
-//   const resp = await axios({
-//     method: "GET",
-//     url: `${BASE_URL}/${CURR_DECK}/draw/?count=1`
-//   });
-
-
-//   console.log(resp.data)
-// }
